dc047be2088fab3dd8c615062676f1e5dccb7b37
Jean Carlos <jhenriquez2204@gmail.com>
2019-12-04 18:55:36 +0000

    Adding a global mapping structure for memoization.

--- main.go
+++ main.go
@@ -4,6 +4,12 @@ import ("fmt"
         "math"
 )
 
+var seen map[int]bool
+
+func init () {
+  seen = make(map[int]bool)
+}
+
 func SquareHappy(n int) (result int) {
   for n > 0 {
     result += int(math.Pow(math.Mod(float64(n), 10), 2))
@@ -14,26 +20,37 @@ func SquareHappy(n int) (result int) {
 }
 
 func isHappy(n int) bool {
+  if value, saw := seen[n]; saw {
+    return value
+  }
 
-  seen := make(map[int]bool)
-  seen[n] = true
+  initN := n
+  cyclePreventer := make(map[int] bool)
 
   for n != 1 {
     n = SquareHappy(n)
-    if _,saw := seen[n]; saw {
-      return false
+    if value, saw := seen[n]; saw {
+      seen[initN] = value
+      return value
     }
 
     if n == 0 {
       return false
     }
 
-    seen[n] = true
+    if _, saw := cyclePreventer[n]; saw {
+      seen[initN] = false
+      return false
+    }
+
+    cyclePreventer[n] = true
+
   }
 
+  seen[initN] = true
   return true
 }
 
 func main() {
-  fmt.Printf("is Happy %v\n", isHappy(7))
+  fmt.Printf("is Happy %v\n", isHappy(2))
 }

b8ea788cf80957022e3a1b03fe0447d5097ce27f
Jean Carlos <jhenriquez2204@gmail.com>
2019-12-04 16:08:59 +0000

    Providing possible solution.

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import ("fmt"
+        "math"
+)
+
+func SquareHappy(n int) (result int) {
+  for n > 0 {
+    result += int(math.Mod(float64(n), 10))
+    n /= 10
+  }
+
+  return
+}
+
+func isHappy(n int) bool {
+
+  if n < 10 {
+    return false
+  }
+
+  seen := make(map[int]bool)
+  seen[n] = true
+
+  for n != 1 {
+    n = SquareHappy(n)
+    if _,saw := seen[n]; saw {
+      return false
+    }
+
+    if n == 0 {
+      return false
+    }
+
+    seen[n] = true
+  }
+
+  return true
+}
 
 func main() {
-  fmt.Println("Hello World")
+  fmt.Printf("is Happy %v\n", isHappy(19))
 }
